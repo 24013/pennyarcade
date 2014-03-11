cbad10df44e9b66017d1dcc522ce4f46c33f39ff
CustomEpicness <tomcharlshaw@gmail.com>
2014-03-11 18:12:53 +0000

    Changed/added stuff

--- java/pennyarcade/entity/villager/VillagerTradeHandler.java
+++ java/pennyarcade/entity/villager/VillagerTradeHandler.java
@@ -18,10 +18,12 @@ public class VillagerTradeHandler implements IVillageTradeHandler {
 		recipeList.add(new MerchantRecipe(new ItemStack(PennyArcade.goldCoin, 25), null, new ItemStack(Items.emerald, 1)));
 		recipeList.add(new MerchantRecipe(new ItemStack(PennyArcade.goldCoin, 25), null, new ItemStack(Items.diamond, 1)));
 		
-		recipeList.add(new MerchantRecipe(new ItemStack(PennyArcade.goldCoin, 50), new ItemStack(Items.emerald, 1), new ItemStack(PennyArcade.emeraldToken, 1)));
+		recipeList.add(new MerchantRecipe(new ItemStack(PennyArcade.goldCoin, 25), new ItemStack(Items.emerald, 1), new ItemStack(PennyArcade.emeraldToken, 1)));
 		
 		recipeList.add(new MerchantRecipe(new ItemStack(PennyArcade.emeraldToken, 3), null, new ItemStack(PennyArcade.miniCreeper, 1)));
 		recipeList.add(new MerchantRecipe(new ItemStack(PennyArcade.emeraldToken, 3), null, new ItemStack(PennyArcade.miniPig, 1)));
+		recipeList.add(new MerchantRecipe(new ItemStack(PennyArcade.emeraldToken, 3), null, new ItemStack(PennyArcade.miniEnderman, 1)));
+		
 		recipeList.add(new MerchantRecipe(new ItemStack(PennyArcade.emeraldToken, 2), null, new ItemStack(PennyArcade.rubixCube, 1)));
 	}
 
